d28fbd9330ec09c77d4b32ac797283f4faeb0072
McGossy <53105381+McGossy@users.noreply.github.com>
2022-01-10 12:26:36 -0500

    day 4 part1 fully working.

--- day4/part1.py
+++ day4/part1.py
@@ -1,9 +1,15 @@
+# this project is a mock-up bingo. Made up of pre-determined numbers to be drawn
+# and pre-determined boards that are in play
+# goal is to draw the numbers one at a time, mark the boards with that number
+# and find the first board to win
+# win condition only checks for 5 in a row, or 5 in a column. No diagonal checks.
 import re
 number_pattern = re.compile(r"\d+")
 lines = ""
 with open('input.txt', 'r') as file:
     lines = file.readlines()
 
+# gather the numbers to be drawn
 draw_pile = []
 matches = number_pattern.finditer(lines[0])
 for match in matches:
@@ -11,22 +17,7 @@ for match in matches:
 del lines[0]
 print(draw_pile)
 
-'''
-board_num = -1
-num = ''
-boards = [[] for i in range(3)]
-# getting all the numbers for the boards
-for i in range(len(lines)):
-    matches = number_pattern.finditer(lines[i])
-    for match in matches:
-        boards[board_num].append(match.group())
-    if lines[i] == '\n':
-        board_num += 1
-print("\n|First gen boards|")
-for board in boards:
-    print(board)
-'''
-
+# Gather every board. Put into 2D list. Boards are represented as a 1D list
 board_num = -1
 boards = []
 b = []
@@ -42,13 +33,14 @@ for i in range(len(lines)):
             for nums in b:
                 boards[board_num].append(nums)
             b = []
+# drop the first element since its a blank list and idk how else to fix that lol
 boards = boards[1::]
 print("\n|new gen boards|")
 for board in boards:
     print(board)
 print("\n|full new boards|\n", boards)
 
-
+# print a list of boards
 def print_boards(boards):
     for i in range(len(boards)):
         print("\n")
@@ -57,7 +49,7 @@ def print_boards(boards):
                 print()
             print(boards[i][j], end=' ')
 
-
+# print one board
 def print_board(board):
     for i in range(len(board)):
         if i % 5 == 0:
@@ -65,32 +57,24 @@ def print_board(board):
         print(board[i], end=' ')
     print()
 
-
-print("\n\nboards")
-print_boards(boards)
-
-
+# checking win conditions
 def check_win(board):
-    print("\nTrying row check")
     for i in range(0, len(board), 5):
         win_count = 0
         for j in range(i + 1, i + 5):
             if '*' in board[i] and '*' in board[j]:
                 win_count += 1
-                print(board[i], "marked, ", board[j], "marked")
             if win_count == 4:
                 return True
-    print("\nTrying column check")
     for i in range(5):
         win_count = 0
         for j in range(i + 5, len(board), 5):
             if '*' in board[i] and '*' in board[j]:
                 win_count += 1
-                print(board[i], "marked, ", board[j], "marked")
             if win_count == 4:
                 return True
 
-
+# draw next number and update all boards if the board has the number drawn
 def draw_and_update_boards(i, draw_pile, boards):
     for j in range(len(boards)):
         for k in range(len(boards[j])):
@@ -98,30 +82,14 @@ def draw_and_update_boards(i, draw_pile, boards):
                 boards[j][k] = "*" + boards[j][k]
     return boards
 
-
-winning_board = []
-winning_num = 0
-for i in range(len(draw_pile)):
-    draw_and_update_boards(i, draw_pile, boards)
-    if winning_num > 0:
-        break
-    for j in range(len(boards)):
-        if check_win(boards[j]):
-            print(f"board[{j}] wins!")
-            winning_board = boards[j]
-            winning_num = int(draw_pile[i])
-
-print('\n\n|WINNING BOARD|')
-print_board(winning_board)
-
-
+# not used. But will remove all the marks on the board
 def remove_marks(board):
     for i in range(len(board)):
         if '*' in board[i]:
             board[i] = board[i][1:]
     return board
 
-
+# ignores the numbers that were picked. Sums up the remaining numbers
 def get_sum_board(board):
     sum = 0
     for i in range(len(board)):
@@ -130,31 +98,26 @@ def get_sum_board(board):
     return sum
 
 
+# main game loop
+def play_game():
+    winning_board = []
+    winning_num = 0
+    for i in range(len(draw_pile)):
+        if winning_num > 0:
+            break
+        draw_and_update_boards(i, draw_pile, boards)
+        for j in range(len(boards)):
+            if check_win(boards[j]):
+                print(f"board[{j}] wins!")
+                winning_board = boards[j]
+                winning_num = int(draw_pile[i])
 
-winning_sum = get_sum_board(winning_board)
-print(winning_sum)
-print(winning_num)
-print("final score = ", winning_sum * winning_num)
+    print('\n\n|WINNING BOARD|')
+    print_board(winning_board)
 
+    winning_sum = get_sum_board(winning_board)
+    print("sum of board: ", winning_sum)
+    print("winning num: ", winning_num)
+    print("final score = ", winning_sum * winning_num)
 
-def check_win_all(boards):
-    winning_boards = []
-    for board in boards:
-        for i in range(0, len(board), 5):
-            win_count = 0
-            for j in range(i + 1, i + 5):
-                if '*' in board[i] and '*' in board[j]:
-                    win_count += 1
-                if win_count == 4:
-                    winning_boards.append(board)
-        for i in range(5):
-            win_count = 0
-            for j in range(i + 5, len(board), 5):
-                if '*' in board[i] and '*' in board[j]:
-                    win_count += 1
-                if win_count == 4:
-                    winning_boards.append(board)
-    return winning_boards
-
-print("\n\nAll winning boards?")
-print_boards(boards)
+play_game()
